b71569a22628d4f7ab4d1b255810e21911297ed4
Fabien Townsend <fabientownsend@gmail.com>
2018-07-22 20:37:58 +0100

    Reads config file from test reporter

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+'use strict';
+
+const {promisify} = require('util');
+const fs = require('fs');
+const readFile = promisify(fs.readFile);
+
 module.exports = function TestRailReporter(action) {
-  action.on('start', test => {});
+  let conf;
+
+  action.on('start', test => {
+    conf = readFile('./testrailrc.json');
+  });
 
   action.on('pass', test => {});
 
   action.on('fail', test => {});
 
-  action.on('end', test => {});
+  action.on('end', test => {
+    conf.then(r => console.log(JSON.parse(r)));
+  });
 };
