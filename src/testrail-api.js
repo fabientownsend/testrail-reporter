232fb9dc000fa49cb34e7e7b2887d7fca082991c
Fabien Townsend <fabientownsend@gmail.com>
2018-07-24 23:58:42 +0100

    Adds post request for results

--- src/testrail-api.js
+++ src/testrail-api.js
@@ -1,9 +1,23 @@
 'use strict';
+const axios = require('axios');
 
 class TestRailApi {
   static sendResults(destination, results) {
+    const testRailPath = `${destination.address}index.php?/api/v2/add_results_for_cases/${destination.runId}`;
+
+    axios({
+      url: testRailPath,
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      auth: {
+        username: destination.login,
+        password: destination.password,
+      },
+      data: JSON.stringify(results),
+    })
+      .then(e => console.log('Hourray'))
+      .catch(error => console.log(`Oh noes ${error}`));
   }
 }
 
 module.exports.TestRailApi = TestRailApi;
-
